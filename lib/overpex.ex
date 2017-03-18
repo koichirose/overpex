3d074971c86c3bf290a0e83b51cd0abb8ae8e8c7
Bruno Santos <brunoasantos2@gmail.com>
2017-03-18 07:04:26 -0300

    Initial commit

---
+++ lib/overpex.ex
@@ -0,0 +1,18 @@
+defmodule Overpex do
+  @moduledoc """
+  Documentation for Overpex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Overpex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/overpex_test.exs
@@ -0,0 +1,8 @@
+defmodule OverpexTest do
+  use ExUnit.Case
+  doctest Overpex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
