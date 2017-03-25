76359704bb7ad659c9b6b3776fd728190aac1002
Bruno Santos <brunoasantos2@gmail.com>
2017-03-25 19:01:30 -0300

    Add hex metadata

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Overpex.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
+     description: description(),
+     package: package(),
      name: "Overpex",
      source_url: "https://github.com/brunoasantos/overpex",
      docs: [main: "Overpex",
@@ -24,5 +26,19 @@ defmodule Overpex.Mixfile do
      {:poison, "~> 3.1.0"},
      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
      {:mock, "~> 0.2", only: :test}]
+  end
+
+  defp description do
+    """
+    Simple wrapper for [Overpass API](https://wiki.openstreetmap.org/wiki/Overpass_API).
+    """
+  end
+
+  defp package do
+    [name: :overpex,
+     maintainers: ["Bruno Santos"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/brunoasantos/overpex",
+              "Docs" => "https://hexdocs.pm/overpex"}]
   end
 end
