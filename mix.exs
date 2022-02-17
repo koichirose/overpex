cb6eb83d3f96425fab8d943f87cdbed8d638f032
sergiodottori <sergio.bottari@dottori.it>
2022-02-17 16:55:47 +0100

    add deps

--- mix.exs
+++ mix.exs
@@ -22,13 +22,17 @@ defmodule Overpex.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.11.0"},
+    [
+	{:httpoison, "~> 0.11.0"},
      {:sweet_xml, "~> 0.6.5"},
      {:poison, "~> 3.1.0"},
      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
      {:exvcr, "~> 0.8", only: :test},
      {:excoveralls, "~> 0.6", only: :test},
-     {:mock, "~> 0.2", only: :test}]
+     {:mock, "~> 0.2", only: :test},
+      {:credo, "~> 1.4", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+	 ]
   end
 
   defp description do
